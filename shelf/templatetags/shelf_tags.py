b41f40561296e3bb9154d47be362ebb3808e6da4
neoslaw <neo228wp4@gmail.com>
2014-12-29 19:50:33 +0100

    29grudniaa

---
+++ shelf/templatetags/shelf_tags.py
@@ -0,0 +1,13 @@
+﻿# -*- coding: utf-8-sig -*-
+from django.template import Library
+
+register = Library()
+
+@register.filter
+def pub_date(date):
+    return date.year
+
+@register.inclusion_tag('tags/show_editions.html')        
+def show_editions(obj):
+    return {'editions' : obj.editions.all()}
+    
